81c21fb3dce6db181d242af912bde0c8771207d4
mattmccoola <61438180+mattmccoola@users.noreply.github.com>
2021-02-05 12:17:52 +1000

    Update PHP_VPC_3Party_Order_DO.php

--- 3Party/PHP_VPC_3Party_Order_DO.php
+++ 3Party/PHP_VPC_3Party_Order_DO.php
@@ -1,41 +1,36 @@
 <?php
 
-// *********************
-// START OF MAIN PROGRAM
-// *********************
-
-// Define Constants
-// ----------------
-// This is secret for encoding the SHA256 hash
-// This secret will vary from merchant to merchant
-// To not create a secure hash, let SECURE_SECRET be an empty string - ""
-
-$securesecret = "";
-
-//Include VPCPaymentConnection.php file
 include('VPCPaymentConnection.php');
 $conn = new VPCPaymentConnection();
 
 
+// This is secret for encoding the SHA256 hash
+// This secret will vary from merchant to merchant
+
+//$secureSecret = "F2C868145B4B468ED1F98B77092FDDD3";
+$secureSecret = $_POST["Secure_Secret"];
+//echo $secureSecret;
 
 
 // Set the Secure Hash Secret used by the VPC connection object
-$conn->setSecureSecret($securesecret);
+$conn->setSecureSecret($secureSecret);
 
 
 // *******************************************
 // START OF MAIN PROGRAM
 // *******************************************
-
+// Sort the POST data - it's important to get the ordering right
+ksort ($_POST);
 
 // add the start of the vpcURL querystring parameters
 $vpcURL = $_POST["virtualPaymentClientURL"];
-$redirectURL = $_POST["virtualPaymentClientURL"];
+
+// This is the title for display
+$title  = $_POST["Title"];
+$custom = $_POST["custom"];
 
 // Remove the Virtual Payment Client URL from the parameter hash as we 
 // do not want to send these fields to the Virtual Payment Client.
-unset($_POST["virtualPaymentClientURL"]); 
-unset($_POST["btnPay"]);
 unset($_POST["virtualPaymentClientURL"]); 
 unset($_POST["SubButL"]);
 unset($_POST["Title"]);
@@ -43,48 +38,32 @@ unset($_POST["Secure_Secret"]);
 unset($_POST["custom"]);
 
 
-// The URL link for the receipt to do another transaction.
-// Note: This is ONLY used for this example and is not required for 
-// production code. You would hard code your own URL into your application.
-
-// Create the request to the Virtual Payment Client which is a URL encoded GET
-// request. Since we are looping through all the data we may as well sort it in
-// case we want to create a secure hash and add it to the VPC data if the
-// merchant secret has been provided.
-
-ksort ($_POST);
-
-// set a parameter to show the first pair in the URL
-$appendAmp = 0;
-
-?>
-
- <body onload="document.order.submit()">
-<!--body-->
-	<form name="order" action="<?php echo($redirectURL); ?>" method="post">
-    <!-- input type="submit" name="submit" value="Continue"/ -->
-    <p>Please wait while your payment is being processed...</p>
-
-<?php
-	$hashinput = "";
-   foreach($_POST as $key => $value) {
-    // create the hash input and URL leaving out any fields that have no value
-    if (strlen($value) > 0) {
-
-?>
-<input type="hidden" name="<?php echo($key); ?>" value="<?php echo($value); ?>"/><br>
-<?php 			
-        if ((strlen($value) > 0) && ((substr($key, 0,4)=="vpc_") || (substr($key,0,5) =="user_"))) {
-		$hashinput .= $key . "=" . $value . "&";
-		}
-    }
+// Add VPC post data to the Digital Order
+foreach($_POST as $key => $value) {
+	if (strlen($value) > 0) {
+		$conn->addDigitalOrderField($key, $value);
+	}
 }
-$hashinput = rtrim($hashinput, "&");
-?>		
-	<!-- attach SecureHash -->
-    <input type="hidden" name="vpc_SecureHash" value="<?php echo(strtoupper(hash_hmac('SHA256', $hashinput, pack('H*',$securesecret)))); ?>"/>
-	<input type="hidden" name="vpc_SecureHashType" value="SHA256">
-</td></tr>
-</table>
-</form>
-</html>
+
+ 
+
+// Add original order HTML so that another transaction can be attempted.
+$conn->addDigitalOrderField("AgainLink", $againLink);
+
+echo "<br><br><b>Query String:</b><br>";
+
+// Obtain a one-way hash of the Digital Order data and add this to the Digital Order
+$secureHash = $conn->hashAllFields();
+//$conn->addDigitalOrderField("Title", $title);
+$conn->addDigitalOrderField("vpc_SecureHash", $secureHash);
+$conn->addDigitalOrderField("vpc_SecureHashType", "SHA256");
+$conn->addDigitalOrderField("custom", $custom);
+
+
+// Obtain the redirection URL and redirect the web browser
+$vpcURL = $conn->getDigitalOrder($vpcURL);
+
+//header("Location: ".$vpcURL);
+echo "<br><br><b>SHA256 Hash:</b><br>$secureHash<br><br><b>Request:</b><br><a href=$vpcURL>$vpcURL</a>";
+
+?>
