a4ad6fca0921abe4ecdf880b04657fe78e9b4ef5
mattmccoola <61438180+mattmccoola@users.noreply.github.com>
2021-02-10 14:03:54 +1000

    Update PHP_VPC_3Party_Order_DO.php

--- 3Party/PHP_VPC_3Party_Order_DO.php
+++ 3Party/PHP_VPC_3Party_Order_DO.php
@@ -1,41 +1,58 @@
 <?php
 
+// *********************
+// START OF MAIN PROGRAM
+// *********************
+
+// Define Constants
+// ----------------
+// This is secret for encoding the SHA256 hash
+// This secret will vary from merchant to merchant
+// To not create a secure hash, let SECURE_SECRET be an empty string - ""
+
+$securesecret = "2B27D8F0B4A344395016F9745F3C0EB0";
+
+//Include VPCPaymentConnection.php file
 include('VPCPaymentConnection.php');
 $conn = new VPCPaymentConnection();
 
 
-// This is secret for encoding the SHA256 hash
-// This secret will vary from merchant to merchant
-
-//$secureSecret = "2B27D8F0B4A344395016F9745F3C0EB0";
-$secureSecret = $_POST["Secure_Secret"];
-//echo $secureSecret;
 
 
 // Set the Secure Hash Secret used by the VPC connection object
-$conn->setSecureSecret($secureSecret);
+$conn->setSecureSecret($securesecret);
 
 
 // *******************************************
 // START OF MAIN PROGRAM
 // *******************************************
-// Sort the POST data - it's important to get the ordering right
-ksort ($_POST);
+
 
 // add the start of the vpcURL querystring parameters
 $vpcURL = $_POST["virtualPaymentClientURL"];
-
-// This is the title for display
-$title  = $_POST["Title"];
-$custom = $_POST["custom"];
+$redirectURL = $_POST["virtualPaymentClientURL"];
 
 // Remove the Virtual Payment Client URL from the parameter hash as we 
 // do not want to send these fields to the Virtual Payment Client.
+unset($_POST["virtualPaymentClientURL"]); 
+unset($_POST["btnPay"]);
 unset($_POST["virtualPaymentClientURL"]); 
 unset($_POST["SubButL"]);
 unset($_POST["Title"]);
 unset($_POST["Secure_Secret"]);
 unset($_POST["custom"]);
+
+
+// The URL link for the receipt to do another transaction.
+// Note: This is ONLY used for this example and is not required for 
+// production code. You would hard code your own URL into your application.
+
+// Create the request to the Virtual Payment Client which is a URL encoded GET
+// request. Since we are looping through all the data we may as well sort it in
+// case we want to create a secure hash and add it to the VPC data if the
+// merchant secret has been provided.
+
+ksort ($_POST);
 
 // set a parameter to show the first pair in the URL
 $appendAmp = 0;
@@ -68,7 +85,6 @@ $hashinput = rtrim($hashinput, "&");
     <input type="hidden" name="hashinput" value="<?php echo($hashinput); ?>"/>
     <input type="hidden" name="vpc_SecureHash" value="<?php echo(strtoupper(hash_hmac('SHA256', $hashinput, pack('H*',$securesecret)))); ?>"/>
     <input type="hidden" name="vpc_SecureHashType" value="SHA256">
-
 </td></tr>
 </table>
 </form>
